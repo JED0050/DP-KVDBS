050634456951f699f624ed32c232e39fba7cf419
JED0050 <honzonarr@seznam.cz>
2024-07-22 02:16:05 +0200

    Spustitelné skripty na VM

---
+++ Tests/SCRIPTS/Automatic_YCSB_test_script.ps1
@@ -0,0 +1,125 @@
+﻿$dbs_name = Read-Host "Choose DBS [redis | aerospike | memcached | riak]"
+$dbs_name = $dbs_name.ToString().ToLower()
+if (-not (($dbs_name -eq "redis") -or ($dbs_name -eq "aerospike") -or ($dbs_name -eq "memcached") -or ($dbs_name -eq "riak"))) {
+    "`tChosen DBS $dbs_name was not found, only [redis | aerospike | memcached | riak] are acceptable"
+    exit 1
+}
+$workload = Read-Host "Choose Workload [a | b | c]"
+$workload = $workload.ToString().ToLower()
+if (-not (($workload -eq "a") -or ($workload -eq "b") -or ($workload -eq "c"))) {
+    "`tChosen workload-$workload was not found, only a-f is acceptable"
+    "`tDefault Workload-a was chosen"
+    $workload = "a"
+}
+$insertonly = Read-Host "Do you want to test only inserting [0 = no | 1 = yes]"
+$insertonly = $insertonly.ToString().ToLower()
+if ($insertonly -eq "yes" -or $insertonly -eq "y") {
+    $insertonly = "1"
+} elseif ($insertonly -eq "no" -or $insertonly -eq "n") {
+    $insertonly = "0"
+} elseif (-not ($insertonly -eq "0" -or $insertonly -eq "1")) {
+    "`tInsert only option must be set to 0 (no) or 1 (yes)"
+    "`tDefault option 0 (no) was chosen"
+    $insertonly = "0"
+}
+$recordcount = Read-Host "Choose number of records [1000-1000000]"
+$recordcount = [int]$recordcount
+if ((-not ($recordcount -is [int])) -or $recordcount -lt 10 -or $recordcount -gt 10000000) {
+    "`tNumber of records is not int, too small or too large"
+    "`tNumber of records was set to default value 10000"
+    $recordcount = 10000
+}
+$operationcount = ""
+if ($insertonly -eq "0"){
+    $operationcount = Read-Host "Choose number of tests [1000-1000000]"
+    $operationcount = [int]$operationcount
+    if ((-not ($operationcount -is [int])) -or $operationcount -lt 100 -or $operationcount -gt 10000000) {
+        "`tNumber of operations is not int, too small or too large"
+        "`tNumber of operations was set to default value 10000"
+        $operationcount = 10000
+    }
+}
+$workload_info = ""
+if ($workload -eq "a") {
+    $workload_info = "(Update-heavy: 50% read, 50% update, distribution zipfian)"
+} elseif ($workload -eq "b") {
+    $workload_info = "(Read-mostly: 95% read, 5% update, distribution zipfian)"
+} elseif ($workload -eq "c") {
+    $workload_info = "(Read-only: 100% read, distribution zipfian)"
+}
+"`nDBS: $dbs_name`t`t`t`tWorkload: $workload $workload_info"
+if ($insertonly -eq "1") {
+    "Insertonly: yes`t`t`tRecord count: $recordcount"
+} else {
+    "Record count: $recordcount`t`tOperation count: $operationcount"
+}
+"`n`n"
+
+if ($dbs_name -eq "redis") {
+    $image_name = "redis:latest"
+    $image_config = "--name my-redis -p 6379:6379"
+    $ycsb_properties = "-p redis.host=127.0.0.1 -p redis.port=6379"
+}elseif ($dbs_name -eq "aerospike"){
+    $image_name = "aerospike:ee-7.0.0.3_1"
+    $image_config = "--name my_aerospike -p 3000:3000"
+    $ycsb_properties = "-p as.namespace=test"
+}elseif ($dbs_name -eq "memcached"){
+    $image_name = "memcached:latest"
+    $image_config = "--name my-memcached -p 11211:11211"
+    $ycsb_properties = "-p memcached.hosts=127.0.0.1 -p memcached.shutdownTimeoutMillis=99999999"
+}elseif ($dbs_name -eq "riak"){
+    $image_name = "basho/riak-kv:latest"
+    $image_config = "--name my-riak -p 8087:8087 -p 8098:8098"
+    $ycsb_properties = ""
+}else{
+    "`tChosen DBS $dbs_name was not found, only [redis | aerospike | memcached | riak] are acceptable"
+    exit 1
+}
+
+$docker_token
+"Pulling and starting latest docker image of " + $dbs_name
+docker pull $image_name
+$docker_token = Invoke-Expression -Command "docker run $image_config -d $image_name"
+"Docker Container token: " + $docker_token
+
+"`n`nWaiting for DBS to be fully prepared"
+if ($dbs_name -eq "riak"){
+    "`tWait 1 minute"
+    Start-Sleep -Seconds 60
+    docker exec my-riak riak-admin bucket-type create ycsb
+    Start-Sleep -Seconds 10
+    docker exec my-riak riak-admin bucket-type activate ycsb
+    Start-Sleep -Seconds 10
+} else {
+    "`tWait 10 seconds"
+    Start-Sleep -Seconds 10
+}
+
+"`n`nRunning YCSB tests of " + $dbs_name
+"Inserting data"
+cd C:\Users\jedlicka\Desktop\YCSB\ycsb-0.17.0\bin  # path to ycsb bin folder
+$load = Invoke-Expression -Command ".\ycsb load $dbs_name -P ..\workloads\workload$workload $ycsb_properties -p recordcount=$recordcount -p threadcount=4"
+$load
+$load | Out-File -FilePath "output.txt"
+if ($insertonly -eq "0"){
+    "`n`nRunning tests"
+    $run = Invoke-Expression -Command ".\ycsb run $dbs_name -P ..\workloads\workload$workload $ycsb_properties -p operationcount=$operationcount -p threadcount=4"
+    $run
+}
+
+cd C:\Users\jedlicka\Desktop\SCRIPTS\Results  # path for test results files
+"`n`nSaving test output files to results folder"
+$currentDateTime = Get-Date -Format "yyyy_MM_dd_HH_mm_ss"
+$load_file_name = ("load_" + $dbs_name + "_" + $currentDateTime + ".txt")
+$load | Out-File -FilePath $load_file_name
+if ($insertonly -eq "0"){
+    $run_file_name = ("run_" + $dbs_name + "_" + $currentDateTime + ".txt")
+    $run | Out-File -FilePath $run_file_name
+}
+
+"`n`nPausing and removing docker image of " + $dbs_name
+docker stop $docker_token
+docker rm $docker_token
+
+"`n"
+pause
---
+++ Tests/SCRIPTS/README.txt
@@ -0,0 +1,38 @@
+Práce se všemi DBS je prováděna prostřednictvím prostředí Docker. DBS se zde instalují, nastavují a spouští.
+Následně se na základě nastavených portů připojí testovací prostředí YCSB k DBS, naplní ho daty a provede požadované testy.
+
+Pro možnost běhu připravených testů je možné využít spustitelný skript PowerShellu (1) nebo ručně vkládat skripty do příkazové řádky (2).
+
+
+1.)
+Pro snažší práci se preferuje první možnost. Spustitelný PowerShell skript se nazývá "Automatic_YCSB_test_script.ps1".
+Pro jeho běh na něj stačí pouze kliknout pravým tlačítkem a vybrat možnost "Run with PowerShell".
+Případně vybrat možnost "Edit" pro spuštění prostředí Windows PowerShell ISE, ve kterém lze skript editovat, debugovat a opětovně spouštět.
+
+Po spuštění skriptu je po uživateli požadováno, aby do konzole zadal několik základních informací.
+- Nejprve název DBS (redis, aerospike, memcached, riak), kterou chce testovat.
+- Následně testový scénář (Workload a, b, c).
+- Poté určí potřebu provádět pouze testování vkládání dat do DBS (insertonly, 0 = ne, 1 = ano).
+- Nakonec zadá počet vložených záznamů do DBS (recordcount = 1000-1000000) a počet prováděných operací (operationscount = 1000-1000000) definovaných vybraným scénářem.
+
+Skript pro vybraný DBS stáhne Docker image, spustí a nastaví Docker container. Následně spustí příkazy "ycsb load" pro vložení dat do DBS a "ycsb run" pro spuštění testů.
+Výsledek testů se zobrazí v konzoli ve formě tabulek. Veškeré výsledky se také ukládají do souborů umístěných ve složce "Results".
+Jsou vytvořeny dva soubory: jeden pro fázi vkládání dat do DBS a druhý pro samotné testování. Soubory nesou název "'dbs_name'_load/run_'datetime'.txt".
+Po dokončení testů se spuštěný Docker container zastaví a vymaže.
+Docker container je přístupný na základě jeho unikátního token_id, které je vypsáno na začátku testů ve výstupu "Docker Container token: 'docker_token'".
+
+Při spouštění skriptu v jiném adresáři než je jeho výchozí umístění je zapotřebí změnit cestu k projektu YCSB (řádek 100) a služce s výsledky (řádek 110).
+
+
+2.)
+Pro potřeby ručního vkládání skriptů do příkazové řádky, ať už cmd nebo PowerShell, jsou veškeré potřebné skripty popsány a k nalezení v textovém souboru "YCSB_Docker_Scripty.txt".
+
+YCSB skripty je zapotřebí spouštět uvnitř složky "bin" umístěné v projektu YCSB.
+Následně je nutné každý Docker container odstranit na konci testování pro možnost vytvoření nového containeru se stejnými porty daného DBS.
+Docker container se odstraní pomocí příkazů "docker stop 'token_id'", "docker rm 'token_id'".
+ID tokenu je k dispozici hned po vytvoření Docker containeru v příkazové řádce (po použití příkazu "docker run ...").
+
+
+
+Docker:	https://www.docker.com/
+YCSB:	https://github.com/brianfrankcooper/YCSB/releases/tag/0.17.0
